3bd1fc0b2e854916bfc2fddde296234772b4361c
Viljar Kärgenberg <viljark@gmail.com>
2017-04-27 10:24:51 +0300

    folder structure

---
+++ server.js
@@ -0,0 +1,3 @@
+// TODO: add sourcemap support
+
+require("./build/server");
---
+++ src/server/server.js
@@ -0,0 +1,9 @@
+const test = "fafa";
+
+console.log(`hello ${test}`);
+
+const obj = {hey: 1};
+
+const obj2 = {...obj, blegh: 2};
+
+console.log(obj2);
