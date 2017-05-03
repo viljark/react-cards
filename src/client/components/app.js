31ba257fe75eba89709e051cd23bbaea92a20023
Viljar Kärgenberg <viljark@gmail.com>
2017-05-03 18:10:48 +0300

    routing and stores

--- src/client/components/app.js
+++ src/client/components/app.js
@@ -1,15 +1,36 @@
 import "./app.scss";
 
 import React from "react";
+import * as A from "../actions";
 import dialogTypes from "./dialogs";
 import {ContainerBase} from "../lib/component";
 
 class AppContainer extends ContainerBase {
 	componentWillMount() {
-		const {stores: {app}} = this.context;
-
+		const {stores: {app}, services: {dispatcher}} = this.context;
+		const {history} = this.props;
+		console.log("router", history);
 		this.subscribe(app.dialogs$, dialogs => this.setState({dialogs}));
+
+		this.subscribe(
+			dispatcher.onSuccess$(A.GAME_JOIN),
+			action => {
+				const path = `/game/${action.gameId}`;
+				if (history.location.pathname === path) {
+					return;
+				}
+				history.push(path);
+			});
+		this.subscribe(
+			dispatcher.onSuccess$(A.LOBBY_JOIN),
+			() => {
+				if (history.location.pathname === "/")
+					return;
+				history.push("/");
+			}
+		);
 	}
+
 	render() {
 		const {Main, Sidebar, match} = this.props;
 
@@ -35,6 +56,7 @@ class AppContainer extends ContainerBase {
 			</div>
 		);
 	}
+
 	_click() {
 		console.log("im clicked");
 	}
--- src/client/components/game.js
+++ src/client/components/game.js
@@ -1,10 +1,21 @@
 import "./game.scss";
+import * as A from "../actions";
 
 import React from "react";
 
 import {ContainerBase} from "../lib/component";
 
 class GameContainer extends ContainerBase {
+	componentWillMount() {
+		const {stores: {app}} = this.context;
+		const {params} = this.props;
+		const gameId = parseInt(params.gameId);
+
+		this.subscribe(app.reconnected$, () => this.request(A.gameJoin(gameId)));
+
+		this.request(A.gameJoin(gameId));
+	}
+
 	render() {
 		return <p>Game 1</p>;
 	}
